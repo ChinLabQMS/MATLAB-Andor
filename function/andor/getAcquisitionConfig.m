923a9b50815bfe85609b24d6189ad2ccedcfdb1f
zmjm <zmjm@uchicago.edu>
2024-02-11 22:49:04 -0600

    modify acquisition app

--- function/andor/getAcquisitionConfig.m
+++ function/andor/getAcquisitionConfig.m
@@ -2,6 +2,8 @@ function [Config] = getAcquisitionConfig(options)
     arguments
         options.num_frames (1,1) double = 1
     end
+    [ret, YPixels, XPixels] = GetDetector();
+    CheckWarning(ret)
 
     [ret, exposure, accumulate] = GetAcquisitionTimings();
     CheckWarning(ret)
@@ -13,6 +15,8 @@ function [Config] = getAcquisitionConfig(options)
 
     Config = struct('num_frames', options.num_frames, ...
                     'exposure', exposure, ...
-                    'accumulate', accumulate);
+                    'accumulate', accumulate, ...
+                    'x_size', XPixels, ...
+                    'y_size', YPixels);
 
 end
--- take_zelux_imges.m
+++ take_zelux_imges.m
@@ -2,12 +2,28 @@
 [tlCameraSDK, tlCamera] = initializeZelux();
 
 %% Acquire images from Zelux
-image = acquireZeluxImage(tlCamera);
+num_image = 0;
+max_image = Inf;
 
-figure
-imagesc(image)
-daspect([1 1 1])
-colorbar
+fig = figure;
+while num_image < max_image
+
+    image_lattice = acquireZeluxImage(tlCamera);
+    image_dmd = acquireZeluxImage(tlCamera);
+    
+    figure(fig)
+    subplot(1,2,1)
+    imagesc(image_lattice)
+    daspect([1 1 1])
+    colorbar
+
+    subplot(1,2,2)
+    imagesc(image_dmd)
+    daspect([1 1 1])
+    colorbar
+
+    drawnow
+end
 
 %% Close Zelux
 closeZelux(tlCameraSDK, tlCamera)
