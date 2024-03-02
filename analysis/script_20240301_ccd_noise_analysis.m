a745882cbb9c5eb0518f281d25d0694d494df146
zmjm <zmjm@uchicago.edu>
2024-03-01 18:34:50 -0600

    minor edits

--- analysis/script_20240301_ccd_noise_analysis.m
+++ analysis/script_20240301_ccd_noise_analysis.m
@@ -1,6 +1,22 @@
-image1 = Data{2}.Image(:,:,100);
+path = 'C:\Users\qmspc\Desktop\NewLabData\2024\03 March\2024-03-01 Camera acquisition test';
 
+Background = struct();
+for exposure = [0, 200, 400, 600, 800, 1000]
+    filename = sprintf('Data_exposure=%dms.mat', exposure);
+    Background.(sprintf('Exposure_%d', exposure)) = load(fullfile(path, filename), 'Data').Data;
+end
+
+%%
+sample = Background.Exposure_0{1}.Image;
+
+mean_image = mean(double(sample), 3);
 figure
-imagesc(image1)
+imagesc(mean_image)
+daspect([1 1 1])
+colorbar
+
+std_image = std(double(sample), 0, 3);
+figure
+imagesc(std_image)
 daspect([1 1 1])
 colorbar
