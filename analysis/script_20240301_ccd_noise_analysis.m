d47cc9c908e3a266bfe7e42cc89ecdad9e17cfa8
zmjm <zmjm@uchicago.edu>
2024-03-08 19:47:34 -0600

    add code for analyzing CCD background noise

--- analysis/script_20240301_ccd_noise_analysis.m
+++ analysis/script_20240301_ccd_noise_analysis.m
@@ -1,4 +1,5 @@
-path = 'C:\Users\qmspc\Desktop\NewLabData\2024\03 March\2024-03-01 Camera acquisition test';
+% path = 'C:\Users\qmspc\Desktop\NewLabData\2024\03 March\2024-03-01 Camera acquisition test';
+path = 'D:\QMS-DATA\2024\03 March\2024-03-01 Camera acquisition test';
 
 Background = struct();
 for exposure = [0, 200, 400, 600, 800, 1000]
@@ -20,3 +21,16 @@ figure
 imagesc(std_image)
 daspect([1 1 1])
 colorbar
+
+%% Re-sturcture the raw background data
+RawBackground = struct('Andor19330', struct(), ...
+                       'Andor19331', struct(), ...
+                       'Zelux', struct());
+RawBackground.Config = struct('Readout', []);
+cameras = fieldnames(RawBackground);
+for i = 1:length(cameras)
+    camera = cameras{i};
+    for exposure = 0:200:1000
+        RawBackground.(camera).(sprintf(''))
+    end
+end
