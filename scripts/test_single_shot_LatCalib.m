c0274e7693cd06d138eb0f2c3cbc5007797e61c0
Xuanhui Mao <xuanhuimao@Xuanhuis-MacBook-Pro.local>
2024-10-15 10:46:34 -0500

    trivial change

--- scripts/test_single_shot_LatCalib.m
+++ scripts/test_single_shot_LatCalib.m
@@ -1,15 +1,13 @@
 %% Loading the raw dataset
 clear; clc;
-
 DataPath = 'data/2024/10 October/20241004/anchor=64_array64_spacing=70_centered_r=20_r=10.mat';
 Data = load(DataPath, "Data").Data;
-
 %% Preprocess the data
 p = Preprocessor;
 Signal = p.processData(Data);
 
 %% Load a lattice pre-calibration
-LatCalib = load('calibration\LatCalib_20241002.mat');
+LatCalib = load('calibration/LatCalib_20241002.mat');
 
 %% Iterate through the dataset and re-calibrate lattice vectors (and center)
 
@@ -17,7 +15,7 @@ CameraList = ["Andor19330", "Andor19331", "Zelux"];
 LabelList = ["Image", "Image", "Lattice"];
 
 result = struct();
-for i = 1: Signal.AcquisitionConfig.NumAcquisitions
+for i = 1:Signal.AcquisitionConfig.NumAcquisitions
     % Iterate through all three cameras
     for j = 1: length(CameraList)
         camera = CameraList(j);
@@ -26,17 +24,17 @@ for i = 1: Signal.AcquisitionConfig.NumAcquisitions
         [signal_box, x_range, y_range] = prepareBox(signal, LatCalib.(camera).R, 500);
 
         % Overlay the lattice sites on images
-        % figure
-        % imagesc(signal)
-        % axis image
-        % lat_corr = Lattice.prepareSite('hex', 'latr', 20);
-        % LatCalib.(camera).plot(lat_corr)
+        %figure
+        %imagesc(signal)
+        %axis image
+        %lat_corr = Lattice.prepareSite('hex', 'latr', 20);
+        %LatCalib.(camera).plot(lat_corr)
         
         LatCalib.(camera).calibrateR(signal_box, x_range, y_range)
         % Store the updated calibration in a result variable
         result(i).(camera + "_R") = LatCalib.(camera).R;
         
-        % LatCalib.(camera).plot(lat_corr)
+        %LatCalib.(camera).plot(lat_corr)
     end
 end
 
