4da8163c0cf57fc67dcb1f4456fdd5521fa3ee58
qmspc <qmspc4@outlook.com>
2025-02-25 22:42:50 -0600

    add test script for dmd mod freq scan

---
+++ test/mod_freq_scan_20250225.m
@@ -0,0 +1,46 @@
+freq = [1, 10, 20, 30, 40, 50, 60, 70, 77, 85, 100, 120, 150];
+x_range = 50:400;
+y_range = 250:650;
+
+p = Preprocessor();
+count1 = zeros(1, length(freq));
+count2 = zeros(1, length(freq));
+for i = 1: length(freq)
+    filename = fullfile('data/2025/02 February/20250225/', sprintf('mod=%dkHz.mat', freq(i)));
+    Data = load(filename).Data;
+
+    Signal = p.process(Data);
+
+    count1(i) = sum(Signal.Andor19330.Image(x_range, y_range, :), 'all');
+    count2(i) = sum(Signal.Andor19330.Image(x_range + 512, y_range, :), 'all');
+end
+
+%%
+Data = load("data/2025/02 February/20250225/solid_gray.mat").Data;
+Signal = p.process(Data);
+gray1 = sum(Signal.Andor19330.Image(x_range, y_range, :), 'all');
+gray2 = sum(Signal.Andor19330.Image(x_range + 512, y_range, :), 'all');
+
+%%
+Data = load("data/2025/02 February/20250225/no_532.mat").Data;
+Signal = p.process(Data);
+bg1 = sum(Signal.Andor19330.Image(x_range, y_range, :), 'all');
+bg2 = sum(Signal.Andor19330.Image(x_range + 512, y_range, :), 'all');
+
+%%
+figure
+plot(freq, count1, 'o-', freq, count2, 'o-', 'LineWidth', 2)
+hold on
+yline(gray1, '--', 'LineWidth', 2)
+yline(gray2, '--', 'LineWidth', 2)
+% plot(1.44, gray1, 'o', 1.44, gray2, 'o')
+legend('counts in second image', 'counts in first image', 'modulation with gray pattern (1.44kHz)')
+xlabel('modulation frequency (kHz)')
+
+%%
+figure
+plot(freq, count1./count2, 'o-', 'LineWidth', 2)
+hold on
+yline(gray1/gray2, '--', 'LineWidth', 2)
+legend('counts ratio', 'modulation with gray pattern (1.44kHz)')
+xlabel('modulation frequency (kHz)')
