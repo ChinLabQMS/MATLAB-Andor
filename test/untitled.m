3dde365e3f17f94691b43186c6e0d2e2e4c88cc7
zmjm <zmjm@uchicago.edu>
2025-03-14 13:33:01 -0500

    modify calibration test script

--- test/untitled.m
+++ test/untitled.m
@@ -10,32 +10,31 @@ signal = Signal.Zelux.Pattern_532(:, :, 1);
 signal2 = mean(Signal.Andor19330.Image, 3);
 
 figure
-subplot(1, 3, 1)
 imagesc2(template)
-subplot(1, 3, 2)
-imagesc2(signal)
-% hold on
-% plot([617, 617 + 1000 * V1(2)], [901, 901 + 1000 * V1(1)], 'LineWidth', 2, 'Color', 'r')
-% plot([617, 617 + 1000 * V2(2)], [901, 901 + 1000 * V2(1)], 'LineWidth', 2, 'Color', 'g')
-
-subplot(1, 3, 3)
-imagesc2(signal2)
 
 %%
-peak_ang = findFFTAngle(signal, 1, linspace(0, 180, 3600), 'ascend', true);
+fft_ang = findFFTAngle(signal, 1, linspace(0, 180, 3600), 'ascend', true);
 
 %%
-peak_pos = findProjDensityPeak(peak_ang, signal, 1: size(signal, 1), 1: size(signal, 2), 1, 10000, true);
+peak_pos = findProjDensityPeak(fft_ang, 2, signal, 1: size(signal, 1), 1: size(signal, 2), 1, 10000, true);
 
-function [peak_pos, proj_density] = findProjDensityPeak(peak_ang, signal, x_range, y_range, bw, num_points, plot_diagnostic)
-    K = [cosd(peak_ang)', sind(peak_ang)'];
+%%
+options.hash_xline = [667, 817];
+options.hash_yline = [666, 816];
+
+function [V, R] = mapLineFeatures(x_lines, y_lines, peak_pos)
+end
+
+function [peak_pos, K, proj_density] = findProjDensityPeak(fft_ang, num_lines, ...
+    signal, x_range, y_range, bw, num_points, plot_diagnostic)
+    K = [cosd(fft_ang)', sind(fft_ang)'];
     [Y, X] = meshgrid(y_range, x_range);
-    Vproj = [X(:), Y(:)] * K;
+    Kproj = [X(:), Y(:)] * K';
     proj_density = cell(2, 2);
-    peak_pos = nan(2, 2);
+    peak_pos = nan(2, num_lines);
     for i = 1: 2
-        [f, xf] = kde(Vproj(:, i), "Weight", signal(:), "Bandwidth", bw, "NumPoints", num_points);
-        peak_pos(i, :) = findPeaks1D(xf, f, 2);
+        [f, xf] = kde(Kproj(:, i), "Weight", signal(:), "Bandwidth", bw, "NumPoints", num_points);
+        peak_pos(i, :) = findPeaks1D(xf, f, num_lines);
         proj_density{i, 1} = xf;
         proj_density{i, 2} = f;
     end
@@ -45,23 +44,24 @@ function [peak_pos, proj_density] = findProjDensityPeak(peak_ang, signal, x_rang
         imagesc2(ax, y_range, x_range, signal);
         hold(ax, "on")
         quiver(ax, (y_range(1) + y_range(end))/2, (x_range(1) + x_range(end))/2, ...
-            K(1, 2), K(1, 1), 1000, 'LineWidth', 2, 'Color', 'r', 'DisplayName', 'K1')
+            K(1, 2), K(1, 1), 400, 'LineWidth', 2, 'Color', 'r', 'DisplayName', 'K1', ...
+            'MaxHeadSize', 10)
         quiver(ax, (y_range(1) + y_range(end))/2, (x_range(1) + x_range(end))/2, ...
-            K(2, 2), K(2, 1), 1000, 'LineWidth', 2, 'Color', 'm', 'DisplayName', 'K2')
+            K(2, 2), K(2, 1), 400, 'LineWidth', 2, 'Color', 'm', 'DisplayName', 'K2', ...
+            'MaxHeadSize', 10)
         legend(ax)
         for i = 1: 2
             subplot(1, 3, i + 1)
             plot(proj_density{i, 1}, proj_density{i, 2})
             hold on
             xline(peak_pos(i, :), '--')
-            title(sprintf('V%d projection', i))
+            title(sprintf('K%d projection', i))
         end
     end
 end
 
-
-% Use kde to find two line features in FFT pattern and get normalized V
-% vectors along the features as a matrix
+% Use kde to find two line features in FFT angular spectrum and extract the
+% angles
 function peak_ang = findFFTAngle(signal, bw, eval_points, peak_order, plot_diagnostic)
     signal_fft = abs(fftshift(fft2(signal)));
     xy_size = size(signal_fft);
@@ -73,11 +73,32 @@ function peak_ang = findFFTAngle(signal, bw, eval_points, peak_order, plot_diagn
     peak_ang = findPeaks1D(xf, f, 2);
     peak_ang = sort(peak_ang, peak_order);
     if plot_diagnostic
-        figure('Name', 'FFT angular spectrum')
-        plot(xf, f)
+        figure('Name', 'FFT angular spectrum (log)')
+        ax1 = subplot(1, 3, 1);
+        imagesc2(ax1, signal_fft)
+        title(ax1, 'Signal FFT')
+        ax2 = subplot(1, 3, 2);
+        imagesc2(ax2, log(signal_fft))
+        title(ax2, 'Signal FFT (log)')
+        for ax = [ax1, ax2]
+            hold(ax, "on")
+            quiver(ax, xy_center(2), xy_center(1), ...
+                sind(peak_ang(1)) * xy_size(2), cosd(peak_ang(1)) * xy_size(1), ...
+                0.5, 'LineWidth', 2, 'Color', 'r', 'DisplayName', 'K1', ...
+                'MaxHeadSize', 1)
+            quiver(ax, xy_center(2), xy_center(1), ...
+                sind(peak_ang(2)) * xy_size(2), cosd(peak_ang(2)) * xy_size(1), ...
+                0.5, 'LineWidth', 2, 'Color', 'm', 'DisplayName', 'K2', ...
+                'MaxHeadSize', 1)
+            legend()
+        end
+        subplot(1, 3, 3)
+        plot(xf, f, 'DisplayName', 'signal density')
         hold on
-        scatter(ang, signal_fft(:) / sum(signal_fft, 'all'), 2)
-        xline(peak_ang, '--')
+        scatter(ang, signal_fft(:) / sum(signal_fft, 'all'), 2, 'DisplayName', 'signal')
+        xline(peak_ang, '--', 'DisplayName', 'peak angle')
+        legend()
+        title('Angular spectrum')
     end
 end
 
